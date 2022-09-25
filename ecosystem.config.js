dc9b8af0702029c0f96b5e47666186e763917cdb
wonsama <wonsama@kakao.com>
2022-09-25 23:48:12 +0900

    edit parse exception

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       script: "./src/app.js",
       env,
       log_date_format: "YYYY-MM-DD HH:mm:ss",
-      cron_restart: "0 0 * * *",
+      cron_restart: "0 * * * *",
     },
   ],
 };
